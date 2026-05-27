76e4acca4039bfab823c327e242a8a5f66fd7f93
Gourav Patidar <gouravpatidar91@gmail.com>
2026-05-27 20:28:34 +0530

    fix(vercel): remove edge runtime to use standard node runtime

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 import handler from '../dist/server/server.js';
 
-export const config = {
-  runtime: 'edge',
-};
-
 export default async function (request) {
   try {
     return await handler.fetch(request);
